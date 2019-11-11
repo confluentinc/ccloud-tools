62fded80c2cde999d046f942d5ce86d818c72cb0
Robin Moffatt <robin@confluent.io>
2019-11-11 12:36:21 +0000

    Upgrade to TF 0.12

--- terraform/gcp/networking.tf
+++ terraform/gcp/networking.tf
@@ -2,10 +2,8 @@
 ################### VPC ###################
 ###########################################
 resource "google_compute_network" "default" {
-
-  name = "${var.global_prefix}"
+  name                    = var.global_prefix
   auto_create_subnetworks = false
-
 }
 
 ###########################################
@@ -13,23 +11,19 @@ resource "google_compute_network" "default" {
 ###########################################
 
 resource "google_compute_subnetwork" "private_subnet" {
-
-  name = "private-subnet-${var.global_prefix}"
-  project = "${var.gcp_project}"
-  region = "${var.gcp_region}"
-  network = "${google_compute_network.default.id}"
+  name          = "private-subnet-${var.global_prefix}"
+  project       = var.gcp_project
+  region        = var.gcp_region
+  network       = google_compute_network.default.id
   ip_cidr_range = "10.0.1.0/24"
-
 }
 
 resource "google_compute_subnetwork" "public_subnet" {
-
-  name = "public-subnet-${var.global_prefix}"
-  project = "${var.gcp_project}"
-  region = "${var.gcp_region}"
-  network = "${google_compute_network.default.id}"
+  name          = "public-subnet-${var.global_prefix}"
+  project       = var.gcp_project
+  region        = var.gcp_region
+  network       = google_compute_network.default.id
   ip_cidr_range = "10.0.2.0/24"
-
 }
 
 ###########################################
@@ -37,77 +31,62 @@ resource "google_compute_subnetwork" "public_subnet" {
 ###########################################
 
 resource "google_compute_firewall" "rest_proxy" {
+  count = var.instance_count["rest_proxy"] >= 1 ? 1 : 0
 
-  count = "${var.instance_count["rest_proxy"] >= 1 ? 1 : 0}"
-  
-  name = "rest-proxy-${var.global_prefix}"
-  network = "${google_compute_network.default.name}"
+  name    = "rest-proxy-${var.global_prefix}"
+  network = google_compute_network.default.name
 
   allow {
-
     protocol = "tcp"
-    ports = ["22", "8082"]
-
+    ports    = ["22", "8082"]
   }
 
   source_ranges = ["0.0.0.0/0"]
-  target_tags = ["rest-proxy-${var.global_prefix}"]
-
+  target_tags   = ["rest-proxy-${var.global_prefix}"]
 }
 
 resource "google_compute_firewall" "kafka_connect" {
+  count = var.instance_count["kafka_connect"] >= 1 ? 1 : 0
 
-  count = "${var.instance_count["kafka_connect"] >= 1 ? 1 : 0}"
-
-  name = "kafka-connect-${var.global_prefix}"
-  network = "${google_compute_network.default.name}"
+  name    = "kafka-connect-${var.global_prefix}"
+  network = google_compute_network.default.name
 
   allow {
-
     protocol = "tcp"
-    ports = ["22", "8083"]
-
+    ports    = ["22", "8083"]
   }
 
   source_ranges = ["0.0.0.0/0"]
-  target_tags = ["kafka-connect-${var.global_prefix}"]
-
+  target_tags   = ["kafka-connect-${var.global_prefix}"]
 }
 
 resource "google_compute_firewall" "ksql_server" {
+  count = var.instance_count["ksql_server"] >= 1 ? 1 : 0
 
-  count = "${var.instance_count["ksql_server"] >= 1 ? 1 : 0}"
-
-  name = "ksql-server-${var.global_prefix}"
-  network = "${google_compute_network.default.name}"
+  name    = "ksql-server-${var.global_prefix}"
+  network = google_compute_network.default.name
 
   allow {
-
     protocol = "tcp"
-    ports = ["22", "8088"]
-
+    ports    = ["22", "8088"]
   }
 
   source_ranges = ["0.0.0.0/0"]
-  target_tags = ["ksql-server-${var.global_prefix}"]
-
+  target_tags   = ["ksql-server-${var.global_prefix}"]
 }
 
 resource "google_compute_firewall" "control_center" {
+  count = var.instance_count["control_center"] >= 1 ? 1 : 0
 
-  count = "${var.instance_count["control_center"] >= 1 ? 1 : 0}"
-
-  name = "control-center-${var.global_prefix}"
-  network = "${google_compute_network.default.name}"
+  name    = "control-center-${var.global_prefix}"
+  network = google_compute_network.default.name
 
   allow {
-
     protocol = "tcp"
-    ports = ["22", "9021"]
-
+    ports    = ["22", "9021"]
   }
 
   source_ranges = ["0.0.0.0/0"]
-  target_tags = ["control-center-${var.global_prefix}"]
-
+  target_tags   = ["control-center-${var.global_prefix}"]
 }
+
--- terraform/gcp/variables.tf
+++ terraform/gcp/variables.tf
@@ -1,46 +1,33 @@
 variable "global_prefix" {
-
   default = "ccloud-tools"
-
 }
 
 variable "gcp_region" {
-
   default = "us-east1"
-
 }
 
 variable "gcp_availability_zones" {
-
-  type = "list"
+  type = list(string)
 
   default = ["us-east1-b", "us-east1-c", "us-east1-d"]
-
 }
 
 variable "instance_count" {
-
-  type = "map"
+  type = map(string)
 
   default = {
-
-    "rest_proxy"       =  1
-    "kafka_connect"    =  1
-    "ksql_server"      =  1
-    "control_center"   =  1
-
+    "rest_proxy"     = 1
+    "kafka_connect"  = 1
+    "ksql_server"    = 1
+    "control_center" = 1
   }
-
 }
 
 variable "confluent_platform_location" {
-
-  default = "http://packages.confluent.io/archive/5.2/confluent-5.2.1-2.12.zip"
-
+  default = "http://packages.confluent.io/archive/5.3/confluent-5.3.1-2.12.zip"
 }
 
 variable "confluent_home_value" {
-
-  default = "/etc/confluent/confluent-5.2.1"
-
+  default = "/etc/confluent/confluent-5.3.1"
 }
+
